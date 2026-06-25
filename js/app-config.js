e86dc984f541990dfcef70fd0c85580dca8846b5
sorukumar <lnsorukumar@gmail.com>
2026-06-25 10:23:14 -0500

    Update footer links in app-config.js

--- js/app-config.js
+++ js/app-config.js
@@ -8,9 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 { name: 'Home', url: 'index.html' },
                 { name: 'Meet the Builders', url: 'directory.html' },
                 { name: 'Influence Map', url: 'network.html' },
-                { name: 'Builder Galaxy', url: 'contributors.html' },
-                { name: 'Roadmap & Feedback', url: 'roadmap.html' }
+                { name: 'Builder Galaxy', url: 'contributors.html' }
             ],
+            footerLinks: [
+                { name: 'Methodology & Definitions', url: 'https://sorukumar.github.io/orange-dev-tracker/methodology.html' }
+            ],
+            feedbackUrl: 'roadmap.html',
             suiteLinks: [
                 { name: 'orange-dev-tracker', url: 'https://sorukumar.github.io/orange-dev-tracker', icon: 'fas fa-chart-line' },
                 { name: 'this-week-in-bitcoin', url: 'https://sorukumar.github.io/this-week-in-bitcoin', icon: 'fas fa-newspaper' }
